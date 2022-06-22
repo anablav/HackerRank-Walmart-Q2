5473ef65dd8ab7c685e6fdf5c5e052f4db401412
anablav <lavanyasukumar10@gmail.com>
2022-06-21 20:29:19 -0700

    Hacker rank test

--- FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
+++ FurnitureFactory/src/main/java/hackerrank/FurnitureOrder.java
@@ -7,43 +7,64 @@ public class FurnitureOrder implements FurnitureOrderInterface {
     /**
      * TODO: Create a map of Furniture items to order quantities
      */
-    Map<Furniture, Integer> map = new HashMap<>();
+    static Map<Furniture, Integer> map = new HashMap<>();
 
     /**
      * Initialize a new mapping of Furniture types to order quantities.
      */
     FurnitureOrder() {
         // TODO: Complete the constructor
-
+        map.put(Furniture.CHAIR, 0);
+        map.put(Furniture.TABLE, 0);
+        map.put(Furniture.COUCH, 0);
         
     }
 
     public void addToOrder(final Furniture type, final int furnitureCount) {
         // TODO: Complete the method
+        if (map.containsKey(type)) {
+            map.put(type, map.get(type) + furnitureCount);
+        } else {
+            map.put(type, furnitureCount);
+        }
     }
 
     public HashMap<Furniture, Integer> getOrderedFurniture() {
         // TODO: Complete the method
-        return null;
+        HashMap<Furniture, Integer> ordered = new HashMap<>();
+        for (Furniture type : map.keySet())  {
+            if (map.get(type) > 0) {
+                ordered.put(type, map.get(type));
+            }
+        }
+        return ordered;
     }
 
     public float getTotalOrderCost() {
         // TODO: Complete the method
-        return -1.0f;
+        float sum = 0.0f;
+        for (Furniture type : map.keySet())  {
+            sum = sum + (map.get(type) * type.cost());
+        }
+        return sum;
     }
 
     public int getTypeCount(Furniture type) {
         // TODO: Complete the method
-        return -1;
+        return map.get(type);
     }
 
     public float getTypeCost(Furniture type) {
         // TODO: Complete the method
-        return -1.0f;
+        System.out.println(map.get(type));
+        return map.get(type) * type.cost();
     }
 
     public int getTotalOrderQuantity() {
-        // TODO: Complete the method
-        return -1;
+        int sum = 0;
+        for (Furniture type : map.keySet())  {
+            sum = sum + map.get(type);
+        }
+        return sum;
     }
 }
